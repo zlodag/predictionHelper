98f296e8e5bc65c9d4d5bca55b5e2d4a301120bf
zlodag <11688686+zlodag@users.noreply.github.com>
2021-07-21 22:13:19 +1200

    Removed tabs permission. Removed default single entry form on home page to avoid confusion

---
+++ removeFormOnHomepageContentScript.js
@@ -0,0 +1,6 @@
+/*
+All prediction entries should be submitted via the multiple prediction entry form, which handles encryption.
+This removes the single prediction entry form displayed on the home page.
+*/
+
+document.querySelector("form#new_prediction").remove();
